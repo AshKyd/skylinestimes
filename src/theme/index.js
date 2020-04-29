0a65501672589d1ea67ec4f4945bc4144fb791f1
Ash Kyd <ash@kyd.com.au>
2020-04-29 17:22:24 +0200

    bunch of errors

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,7 @@
 require('./index.scss');
 
 // Load Netlify if we need it
-if(['#invite_token'].includes(window.location.hash)){
+if(['#invite_token'].includes(window.location.hash.replace(/=.*/,''))){
   const s = document.createElement('script');
   s.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js';
   document.head.appendChild(s);
