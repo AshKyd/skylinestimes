d1769e5485f3cc557b93c5785fe6d5ba2113918b
Ash Kyd <ash@kyd.com.au>
2020-04-29 17:03:43 +0200

    enable admin

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,1 +1,8 @@
 require('./index.scss');
+
+// Load Netlify if we need it
+if(['#invite_token'].includes(window.location.hash)){
+  const s = document.createElement('script');
+  s.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js';
+  document.head.appendChild(s);
+}
