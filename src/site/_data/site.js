f3b25978a942e606d2f010e86f7bb100c8ba43c2
Ash Kyd <ash@kyd.com.au>
2020-05-02 16:40:31 +0200

    visually hidden sidebar + ga

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,11 +1,13 @@
 module.exports = {
-  title: 'Skylines Times',
-  permalink: 'https://skylinestimes.com',
-  "description": "All the news about Cities Skylines and the city building community.",
-  assetRoot: process.env.ASSET_ROOT || '',
-  "author": {
-    "name": "Ash Kyd",
-    "email": "ash@kyd.com.au"
+  title: "Skylines Times",
+  permalink: "https://skylinestimes.com",
+  description:
+    "All the news about Cities Skylines and the city building community.",
+  assetRoot: process.env.ASSET_ROOT || "",
+  author: {
+    name: "Ash Kyd",
+    email: "ash@kyd.com.au"
   },
-  twitter: 'skylinestimes'
+  twitter: "skylinestimes",
+  googleAnalytics: "UA-2547771-26"
 };
