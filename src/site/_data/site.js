60ac61a1b17a39cf5cd056aac64f9ca1a760d517
Ash Kyd <ash@kyd.com.au>
2020-05-02 16:43:09 +0200

    remove unused config

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -3,7 +3,6 @@ module.exports = {
   permalink: "https://skylinestimes.com",
   description:
     "All the news about Cities Skylines and the city building community.",
-  assetRoot: process.env.ASSET_ROOT || "",
   author: {
     name: "Ash Kyd",
     email: "ash@kyd.com.au"
