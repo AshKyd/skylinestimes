6e3bbda70406dc2a9c53e66f8e5a950be64395a1
Ash Kyd <ash@kyd.com.au>
2020-05-03 17:49:10 +0200

    use webp + responsive parallax

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,24 @@
-const moment = require('moment');
+const moment = require("moment");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 
-
-module.exports = function(eleventyConfig){
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addNunjucksFilter("date", (date, modifier) => 
-    `<time datetime="${date.toISOString()}" class="date date--${modifier || 'static'}">
+  eleventyConfig.addNunjucksFilter(
+    "date",
+    (date, modifier) =>
+      `<time datetime="${date.toISOString()}" class="date date--${modifier ||
+        "static"}">
       <span class="date">${moment(date).format("MMMM Do YYYY")}</span>
       <span class="time">${moment(date).format("hh:mm:ss a")}</span>
     </time>`
   );
   eleventyConfig.addWatchTarget("./theme/**/*.{js,scss}");
-  eleventyConfig.setTemplateFormats([
-    "njk",
-    "md",
-    "css",
-    "jpg",
-    "png"
-  ]);
-  
+  eleventyConfig.setTemplateFormats(["njk", "md", "css", "jpg", "png", "webp"]);
+
   return {
     dir: {
       input: "src/site",
       output: "_site"
     }
-  }
-}
+  };
+};
--- src/js/index.js
+++ src/js/index.js
@@ -22,3 +22,25 @@ domReady(() => {
 });
 
 require("./embedExternalLinks");
+
+// Load the webp polyfill if necessary
+const webp = document.querySelectorAll("img[src$=webp]");
+if (webp) {
+  const testImageAlpha =
+    "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==";
+  const img = new Image();
+  img.onerror = function() {
+    webp.forEach(image => {
+      image.classList.add("loading");
+      image.onload = () => image.classList.remove("loading");
+    });
+    const base = "https://unpkg.com/webp-hero@0.0.0-dev.21/dist-cjs/";
+    Promise.all(
+      [base + "polyfills.js", base + "webp-hero.bundle.js"].map(loadScript)
+    ).then(() => {
+      const webpMachine = new window.webpHero.WebpMachine();
+      webpMachine.polyfillDocument();
+    });
+  };
+  img.src = "data:image/webp;base64," + testImageAlpha;
+}
